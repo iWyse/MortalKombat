1a1c08afa6b8dcbad8ed45fcc2f24db88f8de3bf
Danya Wyse <originalwyse@gmail.com>
2024-03-27 23:02:02 +0300

    add jsDoc

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 функцию появления кнопки Reload сделал с задержкой в одну секунду.
 После окончания игры кнопку Random скрываю
+
+Можно ли для сокращения записи делать так
+return className ? $tag.classList.add(className) : null;
+
+вместо такой записи
+if (className) {
+$tag.classList.add(className);
+}
+
+return className
--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const arenas = document.querySelector('.arenas');
 const randomButton = document.querySelector('.button');
+const formFight = document.querySelector('.control');
+
+const HIT = {
+	head: 30,
+	body: 25,
+	foot: 20,
+};
+
+const ATTACK = ['head', 'body', 'foot'];
 
 const attack = function attack() {
 	console.log(`${this.name} Fight...`);
@@ -31,6 +40,12 @@ const player2 = {
 	attack,
 };
 
+/**
+ * Функция создает элемент HTML
+ * @param {string} tag
+ * @param {string} className
+ * @returns {HTMLElement}
+ */
 function createElement(tag, className) {
 	const $tag = document.createElement(tag);
 	if (className) {
@@ -39,7 +54,11 @@ function createElement(tag, className) {
 
 	return $tag;
 }
-
+/**
+ * Функция создает игрока
+ * @param playerObject
+ * @returns {HTMLElement}
+ */
 function createPlayer(player) {
 	const playerEl = createElement('div', player.title);
 	const progressbar = createElement('div', 'progressbar');
@@ -57,7 +76,11 @@ function createPlayer(player) {
 	playerEl.append(progressbar, character);
 	return playerEl;
 }
-
+/**
+ * Функция выводит результат игры, победа или ничья
+ * @param {string} name
+ * @returns {HTMLElement}
+ */
 function showResult(name) {
 	const gameResult = createElement('div', 'gameResult');
 	if (name) {
@@ -68,20 +91,35 @@ function showResult(name) {
 
 	return gameResult;
 }
-
-function getRandom(min, max) {
-	let rand = min + Math.random() * (max + 1 - min);
-	return Math.floor(rand);
+/**
+ * Функция генерации случайных чисел в диапазоне min / max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function getRandom(number) {
+	return number ? Math.ceil(Math.random() * number) : 20;
 }
-
+/**
+ * Функция уменьшает HP у игрока на величину damage
+ * @param {number} damage
+ * @returns {number}
+ */
 function changeHP(damage) {
 	this.hp -= damage;
 }
-
+/**
+ * Функция возвращает элемент здоровья персонажа
+ * @returns {HTMLElement}
+ */
 function elHP() {
 	return document.querySelector(`.player${this.playerID} .life`);
 }
 
+/**
+ * Функция отрисовывает кол-во жизней в %
+ * @returns {HTMLElement}
+ */
 function renderHP() {
 	const statusHP = document.querySelector(`.player${this.playerID} .hpPlayer`);
 	this.elHP().style.width = this.hp + '%';
@@ -93,6 +131,10 @@ function renderHP() {
 	}
 }
 
+/**
+ * Функция создает кнопку перезагрузки игры
+ * @returns {HTMLElement}
+ */
 function createReloadButton() {
 	const reloadWrap = createElement('div', 'reloadWrap');
 	const buttonRestart = createElement('button', 'button');
@@ -102,14 +144,77 @@ function createReloadButton() {
 	});
 	reloadWrap.append(buttonRestart);
 	arenas.append(reloadWrap);
+
+	return reloadWrap;
+}
+/**
+ * Функция - удар противника
+ * @returns {{hit: (string), defence: (string), value: number}}
+ */
+function enemyAttack() {
+	const hit = ATTACK[getRandom(ATTACK.length) - 1];
+	const defence = ATTACK[getRandom(ATTACK.length) - 1];
+	console.log(ATTACK.length);
+	return {
+		value: getRandom(HIT[hit]),
+		hit,
+		defence,
+	};
 }
 
-randomButton.addEventListener('click', function () {
-	player1.changeHP(getRandom(1, 20));
-	player2.changeHP(getRandom(1, 20));
-	player1.renderHP();
-	player2.renderHP();
+/**
+ * Функция - удар игрока
+ * @param {Element} formFight
+ * @returns {{hit: (string), defence: (string), value: number}}
+ */
+function playerAttack() {
+	const attack = {
+		value: 0,
+		hit: '',
+		defence: '',
+	};
+	for (let item of formFight) {
+		if (item.checked && item.name === 'hit') {
+			attack.value = getRandom(HIT[item.value]);
+			attack.hit = item.value;
+		}
 
+		if (item.checked && item.name === 'defence') {
+			attack.defence = item.value;
+		}
+		//Обнуление контролов
+		item.checked = false;
+	}
+
+	return attack;
+}
+
+/**
+ * Функция - бой
+ * @param player1
+ * @param player2
+ */
+function fight(player1, player2) {
+	const enemy = enemyAttack();
+	const player = playerAttack();
+
+	if (enemy.hit !== player.defence) {
+		player1.changeHP(enemy.value);
+		player1.renderHP();
+	}
+
+	if (player.hit !== enemy.defence) {
+		player2.changeHP(player.value);
+		player2.renderHP();
+	}
+}
+
+/**
+ * Функция - Рельзультат боя
+ * @param player1
+ * @param player2
+ */
+function checkResult(player1, player2) {
 	if (player1.hp === 0 || player2.hp === 0) {
 		randomButton.disabled = true;
 
@@ -126,6 +231,14 @@ randomButton.addEventListener('click', function () {
 	} else if (player1.hp === 0 && player2.hp === 0) {
 		arenas.append(showResult());
 	}
+}
+
+formFight.addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	fight(player1, player2);
+
+	checkResult(player1, player2);
 });
 
 arenas.append(createPlayer(player1));
